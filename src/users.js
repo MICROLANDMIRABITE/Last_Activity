57e71d64cc4d0dda9e2ee141a90497071f590900
MICROLANDMIRABITE <143100396+MICROLANDMIRABITE@users.noreply.github.com>
2023-11-22 18:57:41 +0800

    Add files via upload

---
+++ src/users.js
@@ -0,0 +1,173 @@
+const DATA = [
+    {
+      id: 0,
+      name: { 
+        fname: "Arnold James",
+        lname: "Felisilda"
+      },
+      nick: "Nold",
+      course: "BS Information Technoloy",
+      yrsec: "3A",
+      user: "arnoldjames",
+      pass: "arnold123",
+      type: "Student",
+  },
+  {
+      id: 1,
+      name: { 
+        fname: "Nicole James",
+        lname: "Paguican"
+      },
+      nick: "Nics",
+      course: "BS Information Technoloy",
+      yrsec: "3A",
+      user: "nicolejames",
+      pass: "nicole123",
+      type: "Student",
+  },
+  {
+      id: 2,
+      name: { 
+        fname: "Rodel",
+        lname: "Garcia"
+      },
+      nick: "Rodel",
+      course: "BS Information Technoloy",
+      yrsec: "3A",
+      user: "rodel01",
+      pass: "rodel123",
+      type: "Student",
+  },
+  {
+      id: 3,
+      name: { 
+        fname: "Adrian Jane",
+        lname: "Tahil"
+      },
+      nick: "Ja Ne",
+      course: "BS Information Technoloy",
+      yrsec: "3A",
+      user: "jane01",
+      pass: "adrian123",
+      type: "Student",
+  },
+  {
+      id: 4,
+      name: { 
+        fname: "Joana",
+        lname: "Jagunos"
+      },
+      nick: "Joaney",
+      course: "BS Information Technoloy",
+      yrsec: "3A",
+      user: "joana01",
+      pass: "joana123",
+      type: "Student",
+  },
+  {
+      id: 5,
+      name: { 
+        fname: "Marlon",
+        lname: "Carbonilla"
+      },
+      nick: "Marlonskie",
+      course: "BS Information Technoloy",
+      yrsec: "3A",
+      user: "marlon01",
+      pass: "marlon123",
+      type: "Student",
+  },
+  {
+    id: 6,
+    name: { 
+      fname: "Earl Mike",
+      lname: "Sarabia"
+    },
+    nick: "Earl Mike",
+    course: "BS Information Technoloy",
+    yrsec: "3A",
+    user: "mike03",
+    pass: "mike123",
+    type: "Student",
+  },
+  {
+    id: 7,
+    name: { 
+      fname: "Ira Jane",
+      lname: "Renoblas"
+    },
+    nick: "Ira Janey",
+    course: "BS Information Technoloy",
+    yrsec: "3B",
+    user: "irajane03",
+    pass: "ira123",
+    type: "Student",
+  },
+  {
+    id: 8,
+    name: { 
+      fname: "Jerry",
+      lname: "Ale"
+    },
+    nick: "Jerr",
+    course: "",
+    yrsec: "",
+    user: "jerr90",
+    pass: "jerry123",
+    type: "Teacher",
+},
+{
+    id: 9,
+    name: { 
+      fname: "Fermo",
+      lname: "Asufra"
+    },
+    nick: "bords",
+    course: "",
+    yrsec: "",
+    user: "bords12",
+    pass: "fermo123",
+    type: "Teacher",
+},
+{
+  id: 10,
+  name: { 
+    fname: "Catherin Leah",
+    lname: "Gabo"
+  },
+  nick: "Leah",
+  course: "",
+  yrsec: "",
+  user: "cathleah",
+  pass: "leah123",
+  type: "Teacher",
+},
+{
+  id: 11,
+  name: { 
+    fname: "Winzeal",
+    lname: "Agoc"
+  },
+  nick: "Winzeal",
+  course: "",
+  yrsec: "",
+  user: "winzeal08",
+  pass: "winzeal123",
+  type: "Teacher",
+},
+{
+  id: 12,
+  name: { 
+    fname: "Gabrene",
+    lname: "Diaz"
+  },
+  nick: "Gab",
+  course: "",
+  yrsec: "",
+  user: "gabrene09",
+  pass: "gabrene123",
+  type: "Teacher",
+},
+  ];
+
+  export default DATA;
